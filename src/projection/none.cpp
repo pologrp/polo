0e1e2d3d58d25b76592ef9522b28a3d1f4b0d6fa
Arda Aytekin <aytekin@protonmail.com>
2018-02-05 14:20:56 +0100

    Add no projection and no smoothing

---
+++ src/projection/none.cpp
@@ -0,0 +1,31 @@
+#ifndef NOPROJECTION_CPP_
+#define NOPROJECTION_CPP_
+
+namespace kth {
+namespace optim {
+namespace projection {
+
+template <class float_t> struct none {
+  none() = default;
+
+  // Usual suspects; needed due to the protected destructor.
+  none(const none &) = default;
+  none &operator=(const none &) = default;
+  none(none &&) = default;
+  none &operator=(none &&) = default;
+
+  template <class InputIt1, class InputIt2, class OutputIt>
+  OutputIt project(InputIt1 g_first, InputIt1 g_last, InputIt2 xold,
+                   const float_t step, OutputIt xnew) {
+    return xnew;
+  }
+
+protected:
+  ~none() = default;
+};
+
+} // namespace projection
+} // namespace optim
+} // namespace kth
+
+#endif
---
+++ src/smoothing/none.cpp
@@ -0,0 +1,31 @@
+#ifndef NOSMOOTHING_CPP_
+#define NOSMOOTHING_CPP_
+
+namespace kth {
+namespace optim {
+namespace smoothing {
+
+template <class float_t> struct none {
+  none() = default;
+
+  // Usual suspects; needed due to the protected destructor.
+  none(const none &) = default;
+  none &operator=(const none &) = default;
+  none(none &&) = default;
+  none &operator=(none &&) = default;
+
+  template <class InputIt1, class InputIt2, class OutputIt>
+  OutputIt smooth(InputIt1 d_first, InputIt1 d_last, InputIt2 x,
+                  const std::size_t k, OutputIt g) {
+    return g;
+  }
+
+protected:
+  ~none() = default;
+};
+
+} // namespace smoothing
+} // namespace optim
+} // namespace kth
+
+#endif
