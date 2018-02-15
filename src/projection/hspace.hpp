d987f31e2fee97eb9e944b22e40a8a99c5237f29
Arda Aytekin <aytekin@protonmail.com>
2018-02-15 17:16:44 +0100

    Add halfspace projection

---
+++ src/projection/hspace.hpp
@@ -0,0 +1,65 @@
+#ifndef HALFSPACE_HPP_
+#define HALFSPACE_HPP_
+
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+#include "projection/none.hpp"
+#include "utility/blas.hpp"
+
+namespace pbopt {
+namespace projection {
+
+template <class float_t> struct halfspace : private none<float_t> {
+  haflspace() = default;
+
+  halfspace(const halfspace &) = default;
+  halfspace &operator=(const halfspace &) = default;
+  halfspace(halfspace &&) = default;
+  halfspace &operator=(halfspace &&) = default;
+
+  template <class InputIt>
+  void params(InputIt abegin, InputIt aend, const float_t alpha) {
+    a = std::vector<float_t>(abegin, aend);
+    temp = std::vector<float_t>(a.size());
+    this->alpha = alpha;
+    for (const auto val : a)
+      norm += val * val;
+  }
+  void params(std::vector<float_t> a, const float_t alpha) {
+    this->a = std::move(a);
+    temp = std::vector<float_t>(this->a.size());
+    this->alpha = alpha;
+    for (const auto val : this->a)
+      norm += val * val;
+  }
+
+  template <class InputIt> void initialize(InputIt, InputIt) {}
+
+  template <class InputIt1, class InputIt2, class OutputIt>
+  OutputIt project(const float_t step, InputIt1 xold_begin, InputIt1 xold_end,
+                   InputIt2 gbegin, OutputIt xnew_begin) {
+    none<float_t>::project(step, xold_begin, xold_end, gbegin,
+                           std::begin(temp));
+    float_t atx = pbopt::utility::blas<float_t>::dot(a.size(), a.data(), 1,
+                                                     temp.data(), 1);
+    const float_t diff = atx - alpha;
+    return std::transform(std::begin(temp), std::end(temp), std::begin(a),
+                          xnew_begin, [&](const float_t &x, const float_t &a) {
+                            return x - std::max(diff, float_t{0}) / norm * a;
+                          })
+  }
+
+protected:
+  ~halfspace() = default;
+
+private:
+  float_t alpha, norm{0};
+  std::vector<float_t> a, temp;
+};
+
+} // namespace projection
+} // namespace pbopt
+
+#endif
