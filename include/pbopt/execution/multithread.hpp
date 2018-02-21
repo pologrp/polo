1d2f0ced80d12da6ad8bdd18f1a3ed5847c4e5ba
Arda Aytekin <aytekin@protonmail.com>
2018-02-21 10:43:03 +0100

    Add inconsistent-read multi-thread executor

--- include/pbopt/execution/multithread.hpp
+++ include/pbopt/execution/multithread.hpp
@@ -1,10 +1,13 @@
 #ifndef MULTITHREAD_HPP_
 #define MULTITHREAD_HPP_
 
+#include <atomic>
 #include <iterator>
 #include <mutex>
 #include <thread>
 #include <vector>
+
+#include "pbopt/utility/atomic.hpp"
 
 namespace pbopt {
 namespace execution {
@@ -51,8 +54,6 @@ private:
     const float_t *xend{xbegin + dim};
 
     std::vector<float_t> glocal(dim);
-    const float_t *gbegin{glocal.data()};
-    const float_t *gend{gbegin + dim};
 
     while (true) {
       {
@@ -85,6 +86,85 @@ private:
   std::vector<float_t> x, g;
   unsigned int nthreads{std::thread::hardware_concurrency()};
   std::mutex sync;
+};
+
+template <class float_t> struct _select_atomic;
+
+template <> struct _select_atomic<float> {
+  using type = utility::atomic_float;
+};
+
+template <> struct _select_atomic<double> {
+  using type = utility::atomic_double;
+};
+
+template <class float_t> struct inconsistent {
+  using atomic_float_t = typename _select_atomic<float_t>::type;
+
+  inconsistent() = default;
+
+  inconsistent(const inconsistent &) = delete;
+  inconsistent &operator=(const inconsistent &) = delete;
+  inconsistent(inconsistent &&) = default;
+  inconsistent &operator=(inconsistent &&) = default;
+
+protected:
+  void params(const unsigned int nthreads) { this->nthreads = nthreads; }
+
+  template <class InputIt> void initialize(InputIt xbegin, InputIt xend) {
+    x = std::vector<atomic_float_t>(xbegin, xend);
+    g = std::vector<atomic_float_t>(x.size());
+  }
+
+  template <class T, class Func1, class Func2>
+  void solve(T *algptr, Func1 &&loss, Func2 &&terminate) {
+    std::vector<std::thread> workers(nthreads);
+    for (auto &worker : workers)
+      worker = std::thread([&]() { kernel(algptr, loss, terminate); });
+
+    for (auto &worker : workers)
+      worker.join();
+  }
+
+  float_t getf() const { return fval; }
+  std::vector<float_t> getx() const {
+    return std::vector<float_t>(std::begin(x), std::end(x));
+  }
+
+  ~inconsistent() = default;
+
+private:
+  template <class T, class Func1, class Func2>
+  void kernel(T *algptr, Func1 &&loss, Func2 &&terminate) {
+    float_t step, flocal;
+    const std::size_t dim{x.size()};
+
+    std::vector<float_t> xlocal(dim);
+    const float_t *xbegin{xlocal.data()};
+    const float_t *xend{xbegin + dim};
+
+    std::vector<float_t> glocal(dim);
+
+    while (!terminate(k, fval, std::begin(x), std::end(x), std::begin(g))) {
+      std::copy(std::begin(x), std::end(x), std::begin(xlocal));
+
+      flocal = loss(xbegin, xend, glocal.data());
+
+      algptr->grad(std::begin(glocal), std::end(glocal), std::begin(g));
+      algptr->smooth(k, std::begin(xlocal), std::end(xlocal), std::begin(g),
+                     std::begin(g));
+      step = algptr->step(k, fval, std::begin(xlocal), std::end(xlocal),
+                          std::begin(g));
+      algptr->project(step, std::begin(xlocal), std::end(xlocal), std::begin(g),
+                      std::begin(x));
+      k++;
+    }
+  }
+
+  std::atomic<std::size_t> k;
+  atomic_float_t fval;
+  std::vector<atomic_float_t> x, g;
+  unsigned int nthreads{std::thread::hardware_concurrency()};
 };
 
 } // namespace execution
