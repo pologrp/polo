18bb0029330e25007510129234dcf293f64607bf
Arda Aytekin <aytekin@protonmail.com>
2018-03-06 16:33:00 +0100

    Add `solve` signatures to `singlestage`

--- include/polo/algorithm/singlestage.hpp
+++ include/polo/algorithm/singlestage.hpp
@@ -11,15 +11,16 @@
 #include "polo/smoothing/none.hpp"
 #include "polo/step/constant.hpp"
 #include "polo/utility/maxiter.hpp"
+#include "polo/utility/null.hpp"
+#include "polo/utility/sampler.hpp"
 
 namespace polo {
 namespace algorithm {
-
-template <class value_t, template <class> class Gradient = polo::gradient::none,
-          template <class> class StepSize = polo::step::constant,
-          template <class> class Smoothing = polo::smoothing::none,
-          template <class> class Projection = polo::projection::none,
-          template <class> class Execution = polo::execution::serial>
+template <class value_t, template <class> class Gradient = gradient::none,
+          template <class> class StepSize = step::constant,
+          template <class> class Smoothing = smoothing::none,
+          template <class> class Projection = projection::none,
+          template <class> class Execution = execution::serial>
 struct singlestage : public Gradient<value_t>,
                      public StepSize<value_t>,
                      public Smoothing<value_t>,
@@ -60,20 +61,107 @@ struct singlestage : public Gradient<value_t>,
     Execution<value_t>::params(std::forward<Ts>(params)...);
   }
 
-  template <class Func1, class Func2>
-  void solve(Func1 &&loss, Func2 &&terminate) {
-    Execution<value_t>::solve(this, std::forward<Func1>(loss),
-                              std::forward<Func2>(terminate));
+  template <class Loss, class Terminator, class Logger>
+  void solve(Loss &&loss, Terminator &&terminator, Logger &&logger) {
+    Execution<value_t>::solve(this, std::forward<Loss>(loss),
+                              std::forward<Terminator>(terminator),
+                              std::forward<Logger>(logger));
+  }
+  template <class Loss, class Terminator>
+  void solve(Loss &&loss, Terminator &&terminator) {
+    solve(std::forward<Loss>(loss), std::forward<Terminator>(terminator),
+          utility::detail::null{});
+  }
+  template <class Loss> void solve(Loss &&loss) {
+    solve(std::forward<Loss>(loss), utility::terminator::maxiter{100},
+          utility::detail::null{});
   }
 
-  template <class Func> void solve(Func &&loss) {
-    solve(std::forward<Func>(loss), utility::terminator::maxiter{100});
+  template <class Loss, class Sampler, class Terminator, class Logger>
+  void solve(Loss &&loss, utility::sampler::detail::component_sampler_t s,
+             Sampler &&sampler, const std::size_t num_components,
+             Terminator &&terminator, Logger &&logger) {
+    Execution<value_t>::solve(this, std::forward<Loss>(loss),
+                              std::forward<Terminator>(terminator),
+                              std::forward<Logger>(logger), s,
+                              std::forward<Sampler>(sampler), num_components);
+  }
+  template <class Loss, class Sampler, class Terminator>
+  void solve(Loss &&loss, utility::sampler::detail::component_sampler_t s,
+             Sampler &&sampler, const std::size_t num_components,
+             Terminator &&terminator) {
+    solve(std::forward<Loss>(loss), s, std::forward<Sampler>(sampler),
+          num_components, std::forward<Terminator>(terminator),
+          utility::detail::null{});
+  }
+  template <class Loss, class Sampler>
+  void solve(Loss &&loss, utility::sampler::detail::component_sampler_t s,
+             Sampler &&sampler, const std::size_t num_components) {
+    solve(std::forward<Loss>(loss), s, std::forward<Sampler>(sampler),
+          num_components, utility::terminator::maxiter{100},
+          utility::detail::null{});
+  }
+
+  template <class Loss, class Sampler, class Terminator, class Logger>
+  void solve(Loss &&loss, utility::sampler::detail::coordinate_sampler_t s,
+             Sampler &&sampler, const std::size_t num_coordinates,
+             Terminator &&terminator, Logger &&logger) {
+    Execution<value_t>::solve(this, std::forward<Loss>(loss),
+                              std::forward<Terminator>(terminator),
+                              std::forward<Logger>(logger), s,
+                              std::forward<Sampler>(sampler), num_coordinates);
+  }
+  template <class Loss, class Sampler, class Terminator>
+  void solve(Loss &&loss, utility::sampler::detail::coordinate_sampler_t s,
+             Sampler &&sampler, const std::size_t num_coordinates,
+             Terminator &&terminator) {
+    solve(std::forward<Loss>(loss), s, std::forward<Sampler>(sampler),
+          num_coordinates, std::forward<Terminator>(terminator),
+          utility::detail::null{});
+  }
+  template <class Loss, class Sampler>
+  void solve(Loss &&loss, utility::sampler::detail::coordinate_sampler_t s,
+             Sampler &&sampler, const std::size_t num_coordinates) {
+    solve(std::forward<Loss>(loss), s, std::forward<Sampler>(sampler),
+          num_coordinates, utility::terminator::maxiter{100},
+          utility::detail::null{});
+  }
+
+  template <class Loss, class Sampler1, class Sampler2, class Terminator,
+            class Logger>
+  void solve(Loss &&loss, utility::sampler::detail::component_sampler_t s1,
+             Sampler1 &&sampler1, const std::size_t num_components,
+             utility::sampler::detail::coordinate_sampler_t s2,
+             Sampler2 &&sampler2, const std::size_t num_coordinates,
+             Terminator &&terminator, Logger &&logger) {
+    Execution<value_t>::solve(
+        this, std::forward<Loss>(loss), std::forward<Terminator>(terminator),
+        std::forward<Logger>(logger), s1, std::forward<Sampler1>(sampler1),
+        num_components, s2, std::forward<Sampler2>(sampler2), num_coordinates);
+  }
+  template <class Loss, class Sampler1, class Sampler2, class Terminator>
+  void solve(Loss &&loss, utility::sampler::detail::component_sampler_t s1,
+             Sampler1 &&sampler1, const std::size_t num_components,
+             utility::sampler::detail::coordinate_sampler_t s2,
+             Sampler2 &&sampler2, const std::size_t num_coordinates,
+             Terminator &&terminator) {
+    solve(std::forward<Loss>(loss), s1, std::forward<Sampler1>(sampler1),
+          num_components, s2, std::forward<Sampler2>(sampler2), num_coordinates,
+          std::forward<Terminator>(terminator), utility::detail::null{});
+  }
+  template <class Loss, class Sampler1, class Sampler2>
+  void solve(Loss &&loss, utility::sampler::detail::component_sampler_t s1,
+             Sampler1 &&sampler1, const std::size_t num_components,
+             utility::sampler::detail::coordinate_sampler_t s2,
+             Sampler2 &&sampler2, const std::size_t num_coordinates) {
+    solve(std::forward<Loss>(loss), s1, std::forward<Sampler1>(sampler1),
+          num_components, s2, std::forward<Sampler2>(sampler2), num_coordinates,
+          utility::terminator::maxiter{100}, utility::detail::null{});
   }
 
   value_t getf() const { return Execution<value_t>::getf(); }
   std::vector<value_t> getx() const { return Execution<value_t>::getx(); }
 };
-
 } // namespace algorithm
 } // namespace polo
 
