209a13f935f195c1578cf81def1897bb43e51c65
Arda Aytekin <aytekin@protonmail.com>
2018-03-09 12:37:06 +0100

    Fix `solve` signatures for `serial`

--- include/polo/execution/serial.hpp
+++ include/polo/execution/serial.hpp
@@ -2,7 +2,10 @@
 #define SERIAL_HPP_
 
 #include <iterator>
+#include <utility>
 #include <vector>
+
+#include "polo/utility/sampler.hpp"
 
 namespace polo {
 namespace execution {
@@ -20,20 +23,81 @@ protected:
     g = std::vector<value_t>(x.size());
   }
 
-  template <class T, class Func1, class Func2>
-  void solve(T *algptr, Func1 &&loss, Func2 &&terminate) {
-    value_t step;
+  template <class Algorithm, class Loss, class Terminator, class Logger>
+  void solve(Algorithm *alg, Loss &&loss, Terminator &&terminate,
+             Logger &&log) {
     const value_t *xbegin{&x[0]};
 
-    while (!terminate(k, fval, std::begin(x), std::end(x), std::begin(g))) {
-      fval = loss(xbegin, &g[0]);
-      algptr->grad(std::begin(g), std::end(g), std::begin(g));
-      algptr->smooth(k, std::begin(x), std::end(x), std::begin(g),
-                     std::begin(g));
-      step = algptr->step(k, fval, std::begin(x), std::end(x), std::begin(g));
-      algptr->project(step, std::begin(x), std::end(x), std::begin(g),
-                      std::begin(x));
-      k++;
+    while (!std::forward<Terminator>(terminate)(k, fval, std::begin(x),
+                                                std::end(x), std::begin(g))) {
+      fval = std::forward<Loss>(loss)(xbegin, &g[0]);
+      iterate(alg, std::forward<Logger>(log));
+    }
+  }
+
+  template <class Algorithm, class Loss, class Terminator, class Logger,
+            class Sampler>
+  void solve(Algorithm *alg, Loss &&loss, Terminator &&terminate, Logger &&log,
+             utility::sampler::detail::component_sampler_t, Sampler &&sampler,
+             const std::size_t num_components) {
+    const value_t *xbegin{&x[0]};
+    std::vector<std::size_t> components(num_components);
+
+    while (!std::forward<Terminator>(terminate)(k, fval, std::begin(x),
+                                                std::end(x), std::begin(g))) {
+      std::forward<Sampler>(sampler)(std::begin(components),
+                                     std::end(components));
+      fval = std::forward<Loss>(loss)(xbegin, &g[0], &components[0],
+                                      &components[num_components]);
+      iterate(alg, std::forward<Logger>(log));
+    }
+  }
+
+  template <class Algorithm, class Loss, class Terminator, class Logger,
+            class Sampler>
+  void solve(Algorithm *alg, Loss &&loss, Terminator &&terminate, Logger &&log,
+             utility::sampler::detail::coordinate_sampler_t, Sampler &&sampler,
+             const std::size_t num_coordinates) {
+    const value_t *xbegin{&x[0]};
+    std::vector<std::size_t> coordinates(num_coordinates);
+    std::vector<value_t> partial(num_coordinates);
+
+    while (!std::forward<Terminator>(terminate)(k, fval, std::begin(x),
+                                                std::end(x), std::begin(g))) {
+      std::forward<Sampler>(sampler)(std::begin(coordinates),
+                                     std::end(coordinates));
+      fval = std::forward<Loss>(loss)(xbegin, &partial[0], &coordinates[0],
+                                      &coordinates[num_coordinates]);
+      for (std::size_t idx = 0; idx < num_coordinates; idx++)
+        g[coordinates[idx]] = partial[idx];
+      iterate(alg, std::forward<Logger>(log));
+    }
+  }
+
+  template <class Algorithm, class Loss, class Terminator, class Logger,
+            class Sampler1, class Sampler2>
+  void solve(Algorithm *alg, Loss &&loss, Terminator &&terminate, Logger &&log,
+             utility::sampler::detail::component_sampler_t, Sampler1 &&sampler1,
+             const std::size_t num_components,
+             utility::sampler::detail::coordinate_sampler_t,
+             Sampler2 &&sampler2, const std::size_t num_coordinates) {
+    const value_t *xbegin{&x[0]};
+    std::vector<std::size_t> components(num_components),
+        coordinates(num_coordinates);
+    std::vector<value_t> partial(num_coordinates);
+
+    while (!std::forward<Terminator>(terminate)(k, fval, std::begin(x),
+                                                std::end(x), std::begin(g))) {
+      std::forward<Sampler1>(sampler1)(std::begin(components),
+                                       std::end(components));
+      std::forward<Sampler2>(sampler2)(std::begin(coordinates),
+                                       std::end(coordinates));
+      fval = std::forward<Loss>(loss)(
+          xbegin, &partial[0], &components[0], &components[num_components],
+          &coordinates[0], &coordinates[num_coordinates]);
+      for (std::size_t idx = 0; idx < num_coordinates; idx++)
+        g[coordinates[idx]] = partial[idx];
+      iterate(alg, std::forward<Logger>(log));
     }
   }
 
@@ -43,8 +107,20 @@ protected:
   ~serial() = default;
 
 private:
+  template <class Algorithm, class Logger>
+  void iterate(Algorithm *alg, Logger &&log) {
+    alg->grad(std::begin(g), std::end(g), std::begin(g));
+    alg->smooth(k, std::begin(x), std::end(x), std::begin(g), std::begin(g));
+    step_ = alg->step(k, fval, std::begin(x), std::end(x), std::begin(g));
+    alg->project(step_, std::begin(x), std::end(x), std::begin(g),
+                 std::begin(x));
+    std::forward<Logger>(log)(k, fval, std::begin(x), std::end(x),
+                              std::begin(g), std::end(g));
+    k++;
+  }
+
   std::size_t k{1};
-  value_t fval{0};
+  value_t fval{0}, step_{0};
   std::vector<value_t> x, g;
 };
 } // namespace execution
