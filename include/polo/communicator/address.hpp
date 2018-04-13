3ba6453faee1cedd23f7185c1499cd983f43a99b
Arda Aytekin <aytekin@protonmail.com>
2018-04-13 12:04:52 +0200

    Add `ip` and `address` functionality

---
+++ include/polo/communicator/address.hpp
@@ -0,0 +1,76 @@
+#ifndef ADDRESS_HPP_
+#define ADDRESS_HPP_
+
+#include <cstdint>
+#include <exception>
+#include <regex>
+#include <string>
+
+#include <curl/curl.h>
+
+namespace polo {
+namespace communicator {
+struct ip {
+  /* static functions */
+  static ip getexternal() { return getexternal("http://checkip.dyndns.org"); }
+  static ip getexternal(const char *address) {
+    std::string data, addr;
+    CURL *handle = curl_easy_init();
+    CURLcode rc = curl_easy_setopt(handle, CURLOPT_URL, address);
+    rc = curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, ip::callback);
+    rc = curl_easy_setopt(handle, CURLOPT_WRITEDATA, &data);
+    rc = curl_easy_perform(handle);
+    curl_easy_cleanup(handle);
+    if (rc == CURLE_OK)
+      return parse(data.c_str());
+    else
+      throw std::runtime_error("cURL did not succeed.");
+  }
+
+  static ip parse(const char *text) {
+    std::string address{text};
+    std::regex mask{"(25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.(25[0-5]|(?:"
+                    "2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.(25[0-5]|(?:2[0-4]|1{0,1}"
+                    "[0-9]){0,1}[0-9])\\.(25[0-5]"
+                    "|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])"};
+    std::smatch match;
+    if (std::regex_search(address, match, mask) && match.size() == 5)
+      return ip{std::string{match[0].first, match[0].second}};
+    else
+      throw std::runtime_error("Could not parse IP address.");
+  }
+
+  /* non-static functions */
+  ip() = default;
+
+  std::string get() const { return address_; }
+
+private:
+  static std::size_t callback(char *ptr, std::size_t size, std::size_t nmemb,
+                              void *data) {
+    std::string *sptr = static_cast<std::string *>(data);
+    *sptr = std::string(ptr, size * nmemb);
+    return sptr->size();
+  };
+
+  explicit ip(std::string address) : address_{std::move(address)} {}
+
+  std::string address_{"0.0.0.0"};
+};
+
+struct address {
+  address() = default;
+  address(communicator::ip ip, uint16_t port)
+      : ip_{std::move(ip)}, port_{port} {}
+
+  std::string ip() const { return ip_.get(); }
+  uint16_t port() const noexcept { return port_; }
+
+private:
+  communicator::ip ip_;
+  uint16_t port_;
+};
+} // namespace communicator
+} // namespace polo
+
+#endif
