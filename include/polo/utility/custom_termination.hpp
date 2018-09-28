e41620ee5beee1dfec16e09381b66a27109878eb
Martin Biel <martinbiel93@gmail.com>
2018-09-28 10:24:42 +0200

    Add custom termination for higher-level languages

---
+++ include/polo/utility/custom_termination.hpp
@@ -0,0 +1,39 @@
+#ifndef POLO_UTILITY_CUSTOMTERM_HPP_
+#define POLO_UTILITY_CUSTOMTERM_HPP_
+
+namespace polo {
+namespace utility {
+namespace terminator {
+template <class value_t, class index_t> struct custom {
+  using func_t = bool *(*)(const index_t, const value_t, const value_t *,
+                           const value_t *, const value_t *, void *);
+
+  custom(func_t terminate, void *data) {
+    terminate_ = terminate;
+    data_ = data;
+  }
+
+  custom(const custom &) = default;
+  custom &operator=(const custom &) = default;
+  custom(custom &&) = default;
+  custom &operator=(custom &&) = default;
+
+  template <class InputIt1, class InputIt2>
+  bool operator()(const index_t k, const value_t fval, InputIt1 x_begin,
+                  InputIt1 x_end, InputIt2 g_begin) const {
+    return terminate_(k,fval,x_begin,x_end,g_begin);
+  }
+
+protected:
+
+  ~custom() = default;
+
+private:
+  func_t terminate_;
+  void *data_;
+};
+} // namespace terminator
+} // namespace utility
+} // namespace polo
+
+#endif
