9ffe0eebf8997641bfed3802c5ff09a888f5615c
Arda Aytekin <aytekin@protonmail.com>
2018-10-11 11:31:28 +0200

    Bug fixes

--- include/polo/utility/custom_logger.hpp
+++ include/polo/utility/custom_logger.hpp
@@ -5,25 +5,21 @@ namespace polo {
 namespace utility {
 namespace logger {
 template <class value_t, class index_t> struct custom {
-  using func_t = bool *(*)(const index_t, const value_t, const value_t *,
-                           const value_t *, const value_t *, void *);
+  using func_t = void (*)(const index_t, const value_t, const value_t *,
+                          const value_t *, const value_t *, void *);
 
-  custom(func_t log, void *data) {
-    log_ = log;
-    data_ = data;
-  }
+  custom(func_t log, void *data) : log_{log}, data_{data} {}
 
-  template <class InputIt1, class InputIt2>
-  void operator()(const index_t k, const value_t fval, InputIt1 x_begin,
-                  InputIt1 x_end, InputIt2 g_begin) const {
-    log_(k,fval,x_begin,x_end,g_begin,data_);
+  void operator()(const index_t k, const value_t fval, const value_t *x_begin,
+                  const value_t *x_end, const value_t *g_begin) const {
+    log_(k, fval, x_begin, x_end, g_begin, data_);
   }
 
 private:
   func_t log_;
   void *data_;
 };
-} // namespace terminator
+} // namespace logger
 } // namespace utility
 } // namespace polo
 
