1b89348e075a0b0b721515f0f34efb487e3caf69
Arda Aytekin <aytekin@protonmail.com>
2018-10-12 15:18:56 +0200

    Remove CONFIG from `find_package`
    Removed CONFIG from `find_package` friends, as they were redundant for
    ZeroMQ and cereal. Ideally, one should not put CONFIG, at all, in
    `find_package` as CMake first uses Find<package> modules before
    switching to config files generated by CMake installations. We leave the
    handling of Threads, ZeroMQ and cereal to CMake's default behavior, but
    prefer that CMake only searches for config files when it comes to LAPACK.

--- src/polo-config.cmake
+++ src/polo-config.cmake
@@ -1,6 +1,6 @@
 include(CMakeFindDependencyMacro)
 find_dependency(Threads REQUIRED)
 find_dependency(LAPACK REQUIRED NAMES OpenBLAS lapack)
-find_dependency(ZeroMQ REQUIRED CONFIG)
-find_dependency(cereal REQUIRED CONFIG)
+find_dependency(ZeroMQ REQUIRED)
+find_dependency(cereal REQUIRED)
 include("${CMAKE_CURRENT_LIST_DIR}/polo-targets.cmake")
