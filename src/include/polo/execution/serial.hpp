a897911226a3ef0c38f113bb8b9e49e6d6527523
Arda Aytekin <aytekin@protonmail.com>
2019-02-15 15:56:53 +0100

    Fix `serial` executor
    Fixed the `serial` executor to accommodate the interface change in
    `proxgradient`.

--- src/include/polo/execution/serial.hpp
+++ src/include/polo/execution/serial.hpp
@@ -23,91 +23,90 @@ protected:
     fval = 0;
     x = std::vector<value_t>(xbegin, xend);
     g = std::vector<value_t>(x.size());
+    xb = x.data();
+    xe = xb + x.size();
+    xb_c = xb;
+    xe_c = xe;
+    gb = g.data();
+    ge = gb + g.size();
+    gb_c = gb;
+    ge_c = ge;
     return x;
   }
 
-  template <class Algorithm, class Loss, class Terminator, class Logger>
-  void solve(Algorithm *alg, Loss &&loss, Terminator &&terminate,
-             Logger &&logger) {
-    const value_t *xb_c = x.data();
-    const value_t *xe_c = xb_c + x.size();
-
-    value_t *gb = g.data();
-    const value_t *gb_c = gb;
-
+  template <class Algorithm, class Loss, class Encoder, class Terminator,
+            class Logger>
+  void solve(Algorithm *alg, Loss &&loss, Encoder &&encoder,
+             Terminator &&terminate, Logger &&logger) {
     fval = std::forward<Loss>(loss)(xb_c, gb);
+    auto enc = std::forward<Encoder>(encoder)(gb_c, ge_c);
+    enc(gb, ge);
     while (!std::forward<Terminator>(terminate)(k, fval, xb_c, xe_c, gb_c)) {
       iterate(alg, std::forward<Logger>(logger));
       fval = std::forward<Loss>(loss)(xb_c, gb);
+      enc = std::forward<Encoder>(encoder)(gb_c, ge_c);
+      enc(gb, ge);
     }
   }
 
-  template <class Algorithm, class Loss, class Terminator, class Logger,
-            class Sampler>
-  void solve(Algorithm *alg, Loss &&loss, Terminator &&terminate,
-             Logger &&logger, utility::sampler::detail::component_sampler_t,
-             Sampler &&sampler, const index_t num_components) {
-    const value_t *xb_c = x.data();
-    const value_t *xe_c = xb_c + x.size();
-
-    value_t *gb = g.data();
-    const value_t *gb_c = gb;
-
+  template <class Algorithm, class Loss, class Encoder, class Terminator,
+            class Logger, class Sampler>
+  void solve(Algorithm *alg, Loss &&loss, Encoder &&encoder,
+             Terminator &&terminate, Logger &&logger,
+             utility::sampler::detail::component_sampler_t, Sampler &&sampler,
+             const index_t num_components) {
     std::vector<index_t> components(num_components);
     index_t *cb = components.data();
     index_t *ce = cb + components.size();
     const index_t *cb_c = cb;
     const index_t *ce_c = ce;
 
+    std::forward<Sampler>(sampler)(cb, ce);
+    fval = std::forward<Loss>(loss)(xb_c, gb, cb_c, ce_c);
+    auto enc = std::forward<Encoder>(encoder)(gb_c, ge_c);
+    enc(gb, ge);
     while (!std::forward<Terminator>(terminate)(k, fval, xb_c, xe_c, gb_c)) {
+      iterate(alg, std::forward<Logger>(logger));
       std::forward<Sampler>(sampler)(cb, ce);
       fval = std::forward<Loss>(loss)(xb_c, gb, cb_c, ce_c);
-      iterate(alg, std::forward<Logger>(logger));
+      enc = std::forward<Encoder>(encoder)(gb_c, ge_c);
+      enc(gb, ge);
     }
   }
 
-  template <class Algorithm, class Loss, class Terminator, class Logger,
-            class Sampler>
-  void solve(Algorithm *alg, Loss &&loss, Terminator &&terminate,
-             Logger &&logger, utility::sampler::detail::coordinate_sampler_t,
-             Sampler &&sampler, const index_t num_coordinates) {
-    const value_t *xb_c = x.data();
-    const value_t *xe_c = xb_c + x.size();
-
-    const value_t *gb_c = g.data();
-
+  template <class Algorithm, class Loss, class Encoder, class Terminator,
+            class Logger, class Sampler>
+  void solve(Algorithm *alg, Loss &&loss, Encoder &&encoder,
+             Terminator &&terminate, Logger &&logger,
+             utility::sampler::detail::coordinate_sampler_t, Sampler &&sampler,
+             const index_t num_coordinates) {
     std::vector<index_t> coordinates(num_coordinates);
     index_t *cb = coordinates.data();
     index_t *ce = cb + coordinates.size();
     const index_t *cb_c = cb;
     const index_t *ce_c = ce;
 
-    std::vector<value_t> partial(num_coordinates);
-    value_t *pb = partial.data();
-
+    fval = std::forward<Loss>(loss)(xb_c, gb);
+    std::forward<Sampler>(sampler)(cb, ce);
+    auto enc = std::forward<Encoder>(encoder)(gb_c, ge_c, cb_c, ce_c);
+    enc(gb, ge);
     while (!std::forward<Terminator>(terminate)(k, fval, xb_c, xe_c, gb_c)) {
-      std::forward<Sampler>(sampler)(cb, ce);
-      fval = std::forward<Loss>(loss)(xb_c, pb, cb_c, ce_c);
-      for (auto &val : g)
-        val = 0;
-      for (std::size_t idx = 0; idx < num_coordinates; idx++)
-        g[coordinates[idx]] = partial[idx];
       iterate(alg, std::forward<Logger>(logger));
+      fval = std::forward<Loss>(loss)(xb_c, gb);
+      std::forward<Sampler>(sampler)(cb, ce);
+      enc = std::forward<Encoder>(encoder)(gb_c, ge_c, cb_c, ce_c);
+      enc(gb, ge);
     }
   }
 
-  template <class Algorithm, class Loss, class Terminator, class Logger,
-            class Sampler1, class Sampler2>
-  void solve(Algorithm *alg, Loss &&loss, Terminator &&terminate,
-             Logger &&logger, utility::sampler::detail::component_sampler_t,
-             Sampler1 &&sampler1, const index_t num_components,
+  template <class Algorithm, class Loss, class Encoder, class Terminator,
+            class Logger, class Sampler1, class Sampler2>
+  void solve(Algorithm *alg, Loss &&loss, Encoder &&encoder,
+             Terminator &&terminate, Logger &&logger,
+             utility::sampler::detail::component_sampler_t, Sampler1 &&sampler1,
+             const index_t num_components,
              utility::sampler::detail::coordinate_sampler_t,
              Sampler2 &&sampler2, const index_t num_coordinates) {
-    const value_t *xb_c = x.data();
-    const value_t *xe_c = xb_c + x.size();
-
-    const value_t *gb_c = g.data();
-
     std::vector<index_t> components(num_components);
     index_t *compb = components.data();
     index_t *compe = compb + components.size();
@@ -120,19 +119,18 @@ protected:
     const index_t *coorb_c = coorb;
     const index_t *coore_c = coore;
 
-    std::vector<value_t> partial(num_coordinates);
-    value_t *pb = partial.data();
-
+    std::forward<Sampler1>(sampler1)(compb, compe);
+    fval = std::forward<Loss>(loss)(xb_c, gb, compb_c, compe_c);
+    std::forward<Sampler2>(sampler2)(coorb, coore);
+    auto enc = std::forward<Encoder>(encoder)(gb_c, ge_c, coorb_c, coore_c);
+    enc(gb, ge);
     while (!std::forward<Terminator>(terminate)(k, fval, xb_c, xe_c, gb_c)) {
-      std::forward<Sampler1>(sampler1)(compb, compe);
-      std::forward<Sampler2>(sampler2)(coorb, coore);
-      fval = std::forward<Loss>(loss)(xb_c, pb, compb_c, compe_c, coorb_c,
-                                      coore_c);
-      for (auto &val : g)
-        val = 0;
-      for (std::size_t idx = 0; idx < num_coordinates; idx++)
-        g[coordinates[idx]] = partial[idx];
       iterate(alg, std::forward<Logger>(logger));
+      std::forward<Sampler1>(sampler1)(compb, compe);
+      fval = std::forward<Loss>(loss)(xb_c, gb, compb_c, compe_c);
+      std::forward<Sampler2>(sampler2)(coorb, coore);
+      enc = std::forward<Encoder>(encoder)(gb_c, ge_c, coorb_c, coore_c);
+      enc(gb, ge);
     }
   }
 
@@ -144,14 +142,6 @@ protected:
 private:
   template <class Algorithm, class Logger>
   void iterate(Algorithm *alg, Logger &&logger) {
-    value_t *xb = x.data();
-    const value_t *xb_c = xb;
-    const value_t *xe_c = xb_c + x.size();
-
-    value_t *gb = g.data();
-    const value_t *gb_c = gb;
-    const value_t *ge_c = gb_c + g.size();
-
     alg->boost(index_t(0), k, k, gb_c, ge_c, gb);
     alg->smooth(k, k, xb_c, xe_c, gb_c, gb);
     const value_t step = alg->step(k, k, fval, xb_c, xe_c, gb_c);
@@ -162,6 +152,8 @@ private:
 
   index_t k{1};
   value_t fval{0};
+  value_t *xb, *xe, *gb, *ge;
+  const value_t *xb_c, *xe_c, *gb_c, *ge_c;
   std::vector<value_t> x, g;
 };
 } // namespace execution
